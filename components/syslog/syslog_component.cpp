89763f90ebde4e35b6978d40e54ea9d1458ad7cc
KrX3D <KrX3D@users.noreply.github.com>
2025-04-02 22:19:43 +0200

    Update syslog_component.cpp

--- components/syslog/syslog_component.cpp
+++ components/syslog/syslog_component.cpp
@@ -164,9 +164,9 @@ void SyslogComponent::set_enable_direct_logs(bool en) {
 
 void SyslogComponent::set_globally_enabled(bool en) {
     if (this->globally_enabled != en) {
-        ESP_LOGI(ESPHOME_LOG_LEVEL_INFO, "Syslog component: %s -> %s", 
-                 this->globally_enabled ? "enabled" : "disabled", 
-                 en ? "enabled" : "disabled");
+        this->log(ESPHOME_LOG_LEVEL_INFO, "Syslog component: %s -> %s", 
+          this->globally_enabled ? "enabled" : "disabled", 
+          en ? "enabled" : "disabled");
         
         this->globally_enabled = en;
         
