c10116ae9d085c8f878470fa3b78a7d96a503e07
KrX3D <KrX3D@users.noreply.github.com>
2025-04-05 20:18:24 +0200

    Update syslog_component.h

--- components/syslog/syslog_component.h
+++ components/syslog/syslog_component.h
@@ -103,7 +103,7 @@ class SyslogComponent : public Component {
         std::unique_ptr<socket::Socket> socket_ = nullptr;
         struct sockaddr_storage server;
         socklen_t server_socklen;
-        ESPPreferenceObject pref_;
+        uint32_t preference_handle_;
 };
 
 // Custom action to log a message
